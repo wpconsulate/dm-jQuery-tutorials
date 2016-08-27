f10f697f6e23b074b82195064dcf546fe6a2c896
Divyadeep Narang <dd@dmarkweb.com>
2016-08-27 17:21:28 +0530

    added HTML file + click event in JS File

--- jQuery-demo.js
+++ jQuery-demo.js
@@ -4,3 +4,18 @@ $("#button");
 jQuery("#button");
 
 var dmjs = jQuery.noConflict();
+
+
+dmjs("#myButton").click(function(ev){
+    dmjs("#myButton").html("Loading....");
+});
+
+
+dmjs(".button").on('click', function(ev){
+    
+    // dmjs(this).parent()
+
+    dmjs('<button class="button">Click Me</button>').appendTo(dmjs(this).parent());
+
+});
+
